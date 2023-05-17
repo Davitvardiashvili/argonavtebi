94312831cc621de9e8626e422d8b6e5b1427830f
unknown <dato.vardiashvili2001@gmail.com>
2023-05-17 18:45:45 +0400

    first upload

--- back/argo/front/views.py
+++ back/argo/front/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from movement.models import HomePage,ServicesPage,Gallery,AboutPage,ContactPage,SideImages
+import requests
 from .forms import ContactForm
 from django.core.mail import send_mail
+
+
+def send_to_telegram(message):
+
+    apiToken = '6291820566:AAEph2zox4gBu3l6kwYLt8KTdmyPZLvwlj4'
+    chatID = '-932176749'
+    apiURL = f'https://api.telegram.org/bot{apiToken}/sendMessage'
+
+    try:
+        response = requests.post(apiURL, json={'chat_id': chatID, 'text': message})
+        print(response.text)
+    except Exception as e:
+        print(e)
+
 
 
 
@@ -16,19 +31,7 @@ def mainpage(request):
     side_image_3 = SideImages.objects.get(pk=3)
     side_image_4 = SideImages.objects.get(pk=4)
 
-    if request.method == 'POST':
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            name = form.cleaned_data['name']
-            email = form.cleaned_data['email']
-            subject = form.cleaned_data['subject']
-            message = form.cleaned_data['message']
-            mail_subject = f'Contact Form Submission - {subject}'
-            mail_body = f'Name: {name}\nEmail: {email}\nSubject: {subject}\nMessage: {message}'
-            send_mail(mail_subject, mail_body, email, ['davit.vardiashviliwork@gmail.com'])
-            return render(request, 'front/index.html')
-    else:
-        form = ContactForm()
+
 
 
 
@@ -39,11 +42,27 @@ def mainpage(request):
         'services':services,
         'gallery':gallery,
         'abouts':abouts,
-        # 'form':form,
         'contact':contact,
         'side_image_1':side_image_1,
         'side_image_2':side_image_2,
         'side_image_3':side_image_3,
         'side_image_4':side_image_4
     }
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+            entire = f'Subject: {subject}\n' \
+                     f'Email address: {email}\n' \
+                     f'Name: {name}\n' \
+                     f'Message: {message}'
+            send_to_telegram(entire)
+
+            return render(request, 'front/index.html', context)
+    else:
+        form = ContactForm()
+
     return render(request, 'front/index.html', context)
